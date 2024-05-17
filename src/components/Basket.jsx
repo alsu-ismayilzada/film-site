022ae1a14ae0bddc6707b014a6fe966bc4526232
Alsu Ismayilzade <131715293+alsu-ismayilzada@users.noreply.github.com>
2024-05-17 23:17:11 +0400

    update

---
+++ src/components/Basket.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MovieCard from './MovieCard'
+
+const Basket = () => {
+  return (
+    <div>
+        <div>
+            <MovieCard />
+        </div>
+    </div>
+  )
+}
+
+export default Basket
